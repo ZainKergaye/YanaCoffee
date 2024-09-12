d0496440ca646c72edcbc9328f81c90b2455181a
Zain Kergaye <zain4utah@gmail.com>
2024-09-12 12:53:10 -0600

    Added bootstrap

--- flake.nix
+++ flake.nix
@@ -12,10 +12,16 @@
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
       packages = with pkgs; [
-		
+		nodejs_22 # Don't use this, use it the nix way 
+		nodePackages_latest.live-server
+		nodePackages_latest.sass
 	  ];
 
 	  shellHook = ''
+		  #live-server &
+
+		  #sass --watch ./scss/custom.scss ./css/custom.css -q &
+
 		  echo "Welcome to the shell"
 	  '';
 	};
